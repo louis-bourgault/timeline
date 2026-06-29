d1c79942bc12cca945afaa232cd20f7c14b35ca8
louis-bourgault <louis.bourgault2009@gmail.com>
2026-06-29 17:37:20 +0800

    add scroll

--- src/lib/tlmanager.svelte.ts
+++ src/lib/tlmanager.svelte.ts
@@ -1,6 +1,6 @@
 import type { Event, VisibleEvent } from './types';
 
-let sampleEvents: Event[] = [
+const sampleEvents: Event[] = [
 	{
 		id: 1,
 		title: 'Event 1',
@@ -42,7 +42,21 @@ let sampleEvents: Event[] = [
         precision: 'day',
         isBce: false,
         dateDisplay: 'March 1, 2023'
-    }
+    },
+	{
+		id: 4,
+		title: 'Event 4',
+		description: 'Description for Event 4',
+		wikiUrl: 'https://en.wikipedia.org/wiki/Event_4',
+		viewPriority: 4,
+		importance: 0.3,
+		tagIds: [1, 2, 3],
+		startDate: new Date('2023-11-01'),
+		endDate: new Date('2023-11-15'),
+		precision: 'day',
+		isBce: false,
+		dateDisplay: 'April 1, 2023'
+	}
 ];
 
 export class TLManager {
@@ -56,6 +70,7 @@ export class TLManager {
 	public visibleEvents: VisibleEvent[] = $state([]);
     tlDivs: Map<number, {div: HTMLDivElement, shouldDelete: boolean}> = new Map();
     private resizeObserver: ResizeObserver;
+	private scrollObserver: IntersectionObserver;
 
 	public resizeWatcher() {
 		this.height = this.tl.clientHeight;
@@ -67,7 +82,7 @@ export class TLManager {
 
 	constructor(tl: HTMLDivElement) {
 		this.events = $state([]);
-		for (let event of sampleEvents) {
+		for (const event of sampleEvents) {
 			this.events.push(event);
 		}
 		this.tl = tl;
@@ -80,9 +95,16 @@ export class TLManager {
         this.resizeObserver = new ResizeObserver(() => {
             this.resizeWatcher();
         });
-
-        this.resizeObserver.observe(this.tl);
-
+		this.resizeObserver.observe(this.tl);
+		
+		this.tl.addEventListener('wheel', (e) => {
+			e.preventDefault();
+			this.handleScroll(e.deltaX, e.deltaY);
+			this.updateVisible();
+		});
+		
+		
+		
         this.updateVisible();
 	}
 
@@ -90,14 +112,35 @@ export class TLManager {
         this.resizeObserver.disconnect();
     }
 
+	public handleScroll(deltaX: number, deltaY: number) {
+		console.log(`x: ${deltaX}, y: ${deltaY}`);
+		if (deltaY !== 0) {
+			const zoomBy = 1 + deltaY * 0.001; // Adjust zoom sensitivity as needed
+			
+			this.zoomLevel *= zoomBy;
+			const midDate = new Date((this.startViewportDate.getTime() + this.endViewportDate.getTime()) / 2);
+			const totalTime = this.endViewportDate.getTime() - this.startViewportDate.getTime();
+			const newTotalTime = totalTime * zoomBy;
+			this.startViewportDate = new Date(midDate.getTime() - newTotalTime / 2);
+			this.endViewportDate = new Date(midDate.getTime() + newTotalTime / 2);
+		}
+		if (deltaX !== 0) {
+			const totalTime = this.endViewportDate.getTime() - this.startViewportDate.getTime();
+			const timePerPixel = totalTime / this.width;
+			const timeShift = deltaX * timePerPixel;
+			this.startViewportDate = new Date(this.startViewportDate.getTime() + timeShift);
+			this.endViewportDate = new Date(this.endViewportDate.getTime() + timeShift);
+		}
+	}
+
 	public updateVisible() {
         this.visibleEvents.length = 0; //clear this, might be a better way to reuse
-		for (let event of this.events) {
+		for (const event of this.events) {
 			if (event.startDate >= this.startViewportDate) {
 				//event is visible
-				let startX = this.dateToX(event.startDate);
+				const startX = this.dateToX(event.startDate);
 				if (event.endDate) {
-					let endX = this.dateToX(event.endDate);
+					const endX = this.dateToX(event.endDate);
 					if (endX - startX < 10) {
 						//this is too small to show as a range, just do point
 						this.visibleEvents.push({
@@ -123,12 +166,12 @@ export class TLManager {
 			}
 		}
 
-        for (let event of this.visibleEvents) {
+        for (const event of this.visibleEvents) {
             let tlEventElement: HTMLDivElement | undefined;
             let wasFromMap = false;
             if (this.tlDivs.has(event.id)) {
                 //update existing div
-                let record = this.tlDivs.get(event.id);
+                const record = this.tlDivs.get(event.id);
                 tlEventElement = record?.div;
                 wasFromMap = true;
 
@@ -156,7 +199,7 @@ export class TLManager {
                 this.tlDivs.get(event.id)!.shouldDelete = false;
             }
         }
-        for (let [id, record] of this.tlDivs) {
+        for (const [id, record] of this.tlDivs) {
             if (record.shouldDelete) {
                 this.tl.removeChild(record.div);
                 this.tlDivs.delete(id);
@@ -167,9 +210,9 @@ export class TLManager {
 	}
 
 	dateToX(date: Date): number {
-		let totalTime = this.endViewportDate.getTime() - this.startViewportDate.getTime();
-		let timeSinceStart = date.getTime() - this.startViewportDate.getTime();
-		let percentage = timeSinceStart / totalTime;
+		const totalTime = this.endViewportDate.getTime() - this.startViewportDate.getTime();
+		const timeSinceStart = date.getTime() - this.startViewportDate.getTime();
+		const percentage = timeSinceStart / totalTime;
 		return percentage * this.width;
 	}
 }
